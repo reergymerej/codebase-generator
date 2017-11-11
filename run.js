5a9edf8808c40ed1fa79defbae306e8f94c3db76
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-11-11 13:46:25 -0600

    generate dirs

--- run.js
+++ run.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import fs from 'fs'
+import path from 'path'
 import * as app from './index'
 import template from 'lodash.template'
 import mkdirp from 'mkdirp'
@@ -8,12 +9,16 @@ import mkdirp from 'mkdirp'
 console.log('Generating Codebase...')
 
 const base = 5
-const depth = 2
+const depth = 3
 const tree = app.getTree(base, depth)
 
+let dirCount = 0
 app.iterateTree(tree, (node) => {
-  console.log(node.name)
+  const filepath = path.join('out', node.name)
+  mkdirp.sync(filepath)
+  dirCount++
 })
 
+console.log(dirCount)
 // const t = template('', {})
 // console.log(t)
